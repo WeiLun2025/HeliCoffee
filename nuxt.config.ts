e9fb3bf91c392a56d22c0b8c4f705964e5723280
owen1202 <owen051202@gmail.com>
2026-01-12 23:37:49 +0800

    更新 logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    '@pinia/nuxt'
+    '@pinia/nuxt',
   ],
+
+  app: {
+    head: {
+      link: [
+        {
+          // 使用自製的 icon
+          rel: 'icon', type: 'image/png', href: '/heli-logo.png'
+        }
+      ]
+    }
+  },
 
   // 1. 環境變數與 Runtime Config
   // 這是 SheetOps 的核心，我們之後會把 GAS 的 URL 放在這裡
@@ -17,9 +28,9 @@ export default defineNuxtConfig({
   },
 
   // 2. Pinia 設定 (可選，預設即可)
-  // pinia: {
-  //   storesDirs: ['./stores/**'],
-  // },
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
   
   // 讓 TypeScript 檢查更嚴格一些，符合我們的高品質要求
   typescript: {
